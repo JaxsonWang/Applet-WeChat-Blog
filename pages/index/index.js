8d9360a79d82bf89b6cff4adee60b486763b397b
淮城一只猫 <i@iiong.com>
2018-11-20 17:50:46 +0800

    更新代码

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,14 @@
-//index.js
 //获取应用实例
 const app = getApp()
 
 Page({
   data: {
     desc: { image: '/images/index.png', text: '永远年轻，永远热泪盈眶' },
-  }
+  },
+  //事件处理函数
+  toDetail: function() {
+    wx.navigateTo({
+      url: '../detail/detail'
+    })
+  },
 })
